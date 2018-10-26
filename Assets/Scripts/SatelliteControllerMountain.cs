a5a66d1c4dbf79a3e3a9645c8084206155ec90c0
Unknown <slyguggi@gmail.com>
2018-10-26 22:49:53 +0200

    Added two working satellites
    a third one is still WIP

---
+++ Assets/Scripts/SatelliteControllerMountain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SatelliteControllerMountain : MonoBehaviour
+{
+	[SerializeField] private float speed = 10;
+	[SerializeField] private int amplitude = 20;
+
+	private int height = 0;
+	private Rigidbody2D myRigidbody2D;
+
+	void Start()
+	{
+		myRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 myVelocity = Vector2.up;
+		if (height < amplitude)
+		{
+			height++;
+			myVelocity *= speed;
+		}
+		else if (amplitude <= height && height < 2*amplitude)
+		{
+			height++;
+			myVelocity *= -speed;
+		}
+		else
+		{
+			height = 0;
+		}
+
+		myRigidbody2D.velocity = myVelocity + Vector2.left * speed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		print("test");
+		Destroy(other.gameObject);
+	}
+}
---
+++ Assets/Scripts/SatelliteControllerParabole.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SatelliteControllerParabole : MonoBehaviour
+{
+	[SerializeField] private float speed = 10;
+	[SerializeField] private float amplitude = 5;
+	[SerializeField] private float period = 5;
+	private Rigidbody2D myRigidbody2D;
+
+	void Start()
+	{
+		myRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float A = amplitude;
+		float B = (2 * Mathf.PI)/period;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		print("test");
+		Destroy(other.gameObject);
+	}
+}
