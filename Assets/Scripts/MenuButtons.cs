fb890b1c363c73984dca15737c6a5f866281dbb2
Unknown <slyguggi@gmail.com>
2018-10-28 12:20:28 +0100

    Added main menu and game over button sounds + some fixes

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,29 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuButtons : MonoBehaviour {
+public class MenuButtons : MonoBehaviour
+{
+	private AudioSource audio;
+
+	public void Start()
+	{
+		audio = GetComponent<AudioSource>();
+	}
 
     public void PlayGame ()
-    {
+	{
+		audio.Play();
         //Load le prochain niveau
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame ()
     {
-        Application.Quit();
+	    audio.Play();
+		Application.Quit();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Play"))
-            PlayGame();
+	    if (Input.GetButtonDown("Play"))
+	    {
+		    PlayGame();
+	    }
 
-        if (Input.GetButtonDown("Cancel"))
+	    if (Input.GetButtonDown("Cancel"))
         {
-            QuitGame();
+			QuitGame();
         }
     }
 }
